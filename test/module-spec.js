2f412516e42f0b4df766003d0afa4324a8989f16
Brad Leupen <brad@entrinsik.com>
2015-03-09 18:55:44 -0400

    initial commit

---
+++ test/module-spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var should = require('chai').should();
+var lib = require('../');
+
+describe('your module', function () {
+    it('should exist', function () {
+        should.exist(lib);
+    });
+});
